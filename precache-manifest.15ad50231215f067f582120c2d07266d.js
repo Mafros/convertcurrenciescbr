849542a4c48e2752fba432dea2dbd9516e70db98
Mafros <43349854+Mafros@users.noreply.github.com>
2020-07-08 23:36:57 +0400

    Add files via upload

---
+++ precache-manifest.15ad50231215f067f582120c2d07266d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1936e8938dbd17068845c3e2b41f4375",
+    "url": "/convertcurrenciescbr/index.html"
+  },
+  {
+    "revision": "9e16d93a53d7de69b361",
+    "url": "/convertcurrenciescbr/static/css/main.fb0f32b6.chunk.css"
+  },
+  {
+    "revision": "831259c0a49ee883f7aa",
+    "url": "/convertcurrenciescbr/static/js/2.48e90e9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/convertcurrenciescbr/static/js/2.48e90e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e16d93a53d7de69b361",
+    "url": "/convertcurrenciescbr/static/js/main.eb49e1bf.chunk.js"
+  },
+  {
+    "revision": "0135051c72d8cd94bf04",
+    "url": "/convertcurrenciescbr/static/js/runtime-main.eb981899.js"
+  }
+]);
